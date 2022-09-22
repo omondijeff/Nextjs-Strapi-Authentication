ee82a9c1eea9a19c8d5e6551e1bc3dbe27496241
Jeffrey Omondi <11554jeff@gmail.com>
2022-09-22 13:09:20 +0300

    Reset Pages

--- frontend/pages/auth/forgot-password.js
+++ frontend/pages/auth/forgot-password.js
@@ -1,90 +1,100 @@
-import { useState } from 'react';
-import axios from '../../lib/axios';
-import * as Yup from 'yup';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useState } from "react";
+import axios from "../../lib/axios";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const ForgotPassword = () => {
-    const [alert,setAlert] = useState();
+  const [alert, setAlert] = useState();
 
-    const initialValues = {
-        email: ""
-    }
+  const initialValues = {
+    email: "",
+  };
 
-    const validationSchema = Yup.object({
-        email: Yup.string().email("Insert a valid email").required("Required")
-    });
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Insert a valid email").required("Required"),
+  });
 
-    const onSubmit = (values, { setSubmitting, resetForm }) => {
-        setAlert();
+  const onSubmit = (values, { setSubmitting, resetForm }) => {
+    setAlert();
 
-        axios
-            .post('/auth/forgot-password', values)
-            .then(response => {
-                const message = `Please check your email to reset your password.`;
-                setAlert(['success', message]);
+    axios
+      .post("api/auth/forgot-password", values)
+      .then((response) => {
+        const message = `Please check your email to reset your password.`;
+        setAlert(["success", message]);
 
-                resetForm();
-            })
-            .catch(error => {
-                if ( !error.response.data.message ) {
-                    setAlert(['alert', "Something went wrong"])
-                } else {
-                    const messages = error.response.data.message[0].messages;
+        resetForm();
+      })
+      .catch((error) => {
+        if (!error.response.data.message) {
+          setAlert(["alert", "Something went wrong"]);
+        } else {
+          const messages = error.response.data.message[0].messages;
 
-                    const list = [];
-                    messages.map((message,i) => {
-                        let item = "";
-                        if (i === 0) item += `<ul>`;
-                        
-                        item += `<li>${message.id}</li>`;
+          const list = [];
+          messages.map((message, i) => {
+            let item = "";
+            if (i === 0) item += `<ul>`;
 
-                        if (i === messages.length - 1) item += `</ul>`
-                        list.push(item);
-                    });
+            item += `<li>${message.id}</li>`;
 
-                    setAlert(['alert', list]);
-                }
-            })
-            .finally(() => {
-                setSubmitting(false);
-            });
-    }
+            if (i === messages.length - 1) item += `</ul>`;
+            list.push(item);
+          });
 
-    return (
-        <>
-            <h1>Forgot password</h1>
-            <hr />
-            {alert && (
-                <div style={{ backgroundColor: alert[0] === "success" ? "lightgreen" : "lightcoral" }}>
-                    <div dangerouslySetInnerHTML={{ __html: alert[1] }} />
-                </div>
-            )}
+          setAlert(["alert", list]);
+        }
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <>
+      <h1>Forgot password</h1>
+      <hr />
+      {alert && (
+        <div
+          style={{
+            backgroundColor:
+              alert[0] === "success" ? "lightgreen" : "lightcoral",
+          }}
+        >
+          <div dangerouslySetInnerHTML={{ __html: alert[1] }} />
+        </div>
+      )}
+      <br />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) =>
+          onSubmit(values, { setSubmitting, resetForm })
+        }
+      >
+        {({ isSubmitting, isValid }) => (
+          <Form>
+            <div>
+              <div>
+                <label htmlFor="email">Email</label>
+              </div>
+              <Field type="email" id="email" name="email" placeholder="Email" />
+              <div className="error">
+                <ErrorMessage name="email" />
+              </div>
+            </div>
+
             <br />
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={(values, { setSubmitting, resetForm }) => onSubmit(values, { setSubmitting, resetForm })} >
-                { ({ isSubmitting, isValid }) => (
-                    <Form>
-                        <div>
-                            <div><label htmlFor="email">Email</label></div>
-                            <Field type="email" id="email" name="email" placeholder="Email" />
-                            <div className="error"><ErrorMessage name="email" /></div>
-                        </div>
 
-                        <br />
-
-                        <button 
-                            type="submit"
-                            disabled={!isValid} >
-                            {!isSubmitting && "Send link"}
-                            {isSubmitting && "Loading..."}
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </>
-    )
-}
+            <button type="submit" disabled={!isValid}>
+              {!isSubmitting && "Send link"}
+              {isSubmitting && "Loading..."}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
 
 export default ForgotPassword;
--- frontend/pages/auth/reset-password.js
+++ frontend/pages/auth/reset-password.js
@@ -1,106 +1,134 @@
-import { useState } from 'react';
-import axios from '../../lib/axios';
-import * as Yup from 'yup';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import axios from "../../lib/axios";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useRouter } from "next/router";
 
 const ResetPassword = () => {
-    const { push, query } = useRouter();
-    const [alert,setAlert] = useState();
+  const { push, query } = useRouter();
+  const [alert, setAlert] = useState();
 
-    const initialValues = {
-        password: "",
-        passwordConfirmation: "",
-    }
+  const initialValues = {
+    password: "",
+    passwordConfirmation: "",
+  };
 
-    const validationSchema = Yup.object({
-        password: Yup.string().required("Required"),
-        passwordConfirmation: Yup.string().oneOf([Yup.ref("password"), null], "Passwords must match").required("Required"),
-    });
+  const validationSchema = Yup.object({
+    password: Yup.string().required("Required"),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required"),
+  });
 
-    const onSubmit = (values, { setSubmitting, resetForm }) => {
-        setAlert();
+  const onSubmit = (values, { setSubmitting, resetForm }) => {
+    setAlert();
 
-        values.code = query.code;
+    values.code = query.code;
 
-        axios
-            .post('auth/reset-password', values)
-            .then(response => {
-                const message = `Your password has been resetted. In a few second you'll be redirected to login page.`;
-                setAlert(['success', message]);
+    axios
+      .post("api/auth/reset-password", values)
+      .then((response) => {
+        const message = `Your password has been reset Successfully. Please Go to the Applicaion and Log In.`;
+        setAlert(["success", message]);
 
-                resetForm();
-                
-                setTimeout(() => { push('/auth/login') }, 5000);
-            })
-            .catch(error => {
-                if ( !error.response.data.message ) {
-                    setAlert(['alert', "Something went wrong"])
-                } else {
-                    const messages = error.response.data.message[0].messages;
+        resetForm();
 
-                    const list = [];
-                    messages.map((message,i) => {
-                        let item = "";
-                        if (i === 0) item += `<ul>`;
-                        
-                        item += `<li>${message.id}</li>`;
+        // setTimeout(() => {
+        //   push("/auth/login");
+        // }, 5000);
+      })
+      .catch((error) => {
+        if (!error.response.data.message) {
+          setAlert(["alert", "Something went wrong"]);
+        } else {
+          const messages = error.response.data.message[0].messages;
 
-                        if (i === messages.length - 1) item += `</ul>`
-                        list.push(item);
-                    });
+          const list = [];
+          messages.map((message, i) => {
+            let item = "";
+            if (i === 0) item += `<ul>`;
 
-                    setAlert(['alert', list]);
-                }
-            })
-            .finally(() => {
-                setSubmitting(false);
-            }); 
-    }
+            item += `<li>${message.id}</li>`;
 
-    return (
-        <>
-            <h1>Reset password</h1>
-            <hr />
-            {alert && (
-                <div style={{ backgroundColor: alert[0] === "success" ? "lightgreen" : "lightcoral" }}>
-                    <div dangerouslySetInnerHTML={{ __html: alert[1] }} />
-                </div>
-            )}
+            if (i === messages.length - 1) item += `</ul>`;
+            list.push(item);
+          });
+
+          setAlert(["alert", list]);
+        }
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <>
+      <h1>Reset password</h1>
+      <hr />
+      {alert && (
+        <div
+          style={{
+            backgroundColor:
+              alert[0] === "success" ? "lightgreen" : "lightcoral",
+          }}
+        >
+          <div dangerouslySetInnerHTML={{ __html: alert[1] }} />
+        </div>
+      )}
+      <br />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) =>
+          onSubmit(values, { setSubmitting, resetForm })
+        }
+      >
+        {({ isSubmitting, isValid }) => (
+          <Form>
+            <div>
+              <div>
+                <label htmlFor="password">Password</label>
+              </div>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <div className="error">
+                <ErrorMessage name="password" />
+              </div>
+            </div>
+
             <br />
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={(values, { setSubmitting, resetForm }) => onSubmit(values, { setSubmitting, resetForm })} >
-                { ({ isSubmitting, isValid }) => (
-                    <Form>
-                        <div>
-                            <div><label htmlFor="password">Password</label></div>
-                            <Field type="password" id="password" name="password" placeholder="Password" />
-                            <div className="error"><ErrorMessage name="password" /></div>
-                        </div>
 
-                        <br />
+            <div>
+              <div>
+                <label htmlFor="passwordConfirmation">Repeat Password</label>
+              </div>
+              <Field
+                type="password"
+                id="passwordConfirmation"
+                name="passwordConfirmation"
+                placeholder="Repeat password"
+              />
+              <div className="error">
+                <ErrorMessage name="passwordConfirmation" />
+              </div>
+            </div>
 
-                        <div>
-                            <div><label htmlFor="passwordConfirmation">Repeat Password</label></div>
-                            <Field type="password" id="passwordConfirmation" name="passwordConfirmation" placeholder="Repeat password" />
-                            <div className="error"><ErrorMessage name="passwordConfirmation" /></div>
-                        </div>
+            <br />
 
-                        <br />
-
-                        <button 
-                            type="submit"
-                            disabled={!isValid} >
-                            {!isSubmitting && "Reset password"}
-                            {isSubmitting && "Loading..."}
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </>
-    )
-}
+            <button type="submit" disabled={!isValid}>
+              {!isSubmitting && "Reset password"}
+              {isSubmitting && "Loading..."}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
 
 export default ResetPassword;
